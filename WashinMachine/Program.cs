d2528ef9159ae5e97d325759650c9a848a270c32
Patrick Heesch (patr371f) <patr371f@zbc.dk>
2020-08-28 10:10:15 +0200

    Add project files.

---
+++ WashinMachine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WashinMachine
+{
+    class Program
+    {
+        private static bool powerState; // Makes the bool variable named powerState
+        public static bool PowerState { get { return powerState; } set { powerState = false; } } // Makes it possible to change the value in the private bool powerState later on
+
+
+        public static void Main(string[] args)
+        {
+            WashProgram wash = new WashProgram(); // Calls the WashProgram class and gives it a new name, wash, in this class.
+            powerState = true; //Gives the variable powerState a value so it's always true
+
+            while (true) // A while loop, that, while true, will continue to run
+            {
+               if (powerState == true) // and if statement, that when it is true, will run its content
+               {
+                   Console.WriteLine("Please choose a program"); // A console.writeline that will printout/display the given message in its content
+                   wash.ProgramWash(); // calls on the function wash and runs it
+                   
+               }
+                Console.ReadKey(); // Makes it so you have to press a key in order to proceed in the program
+            }
+        }
+    }
+}
---
+++ WashinMachine/WashProgram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WashinMachine
+{
+    class WashProgram
+    {
+       // public int WashProgarm { get; set; }
+        int choice; 
+        public void ProgramWash() // a Method named ProgramWash
+        {
+            bool onOff = Program.PowerState; // Calls on the bool named powerState and renames it in this part of the program to onOff
+            if (onOff == true) // and if statement, that when true will run its contents
+            {
+                Console.WriteLine("Press 1 for White wash\n Press 2 for Black wash\n Press 3 for Mixed wash"); // A method that displays the given message, in this case which program to choose
+                choice = int.Parse(Console.ReadLine()); // Converts the variable ¨Choice¨ content/input to integer.
+                switch (choice) // Switch case that reads which option you take. 
+                {
+                    case 1:
+                        Console.WriteLine("White clothes wash initiated\n Done in 55 minutes"); // A method that displays the given message, an initiate message with a time until done
+                        break; 
+                    case 2:
+                        Console.WriteLine("Black clothes wash initiated\n Done in 47 minutes"); // A method that displays the given message, an initiate message with a time until done
+                        break;
+                    case 3:
+                        Console.WriteLine("Mixed clothes wash initiated\n Done in 72 minutes"); // A method that displays the given message, an initiate message with a time until done
+                        break;
+                    default:
+                        Console.WriteLine("Error"); // A method that displays the given message
+                        break;
+                }
+
+            }
+        }
+    }
+}
